74835a247e15379467a76eef7e4b345ae4fb400a
Shubhamcypher <79666913+Shubhamcypher@users.noreply.github.com>
2023-10-20 18:16:29 +0000

    data_types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,22 @@
+"use strict";//use all JS code as newer version
+
+//alert(6+6) //We are using nodejs not browser
+
+
+console.log("hey"
+); //Always keep in mind the code readability
+
+/*Types of datatypes
+1. number=> 2 to the power 53
+2. bigint=> for very large number
+3. boolean=> true/false
+4. null=> standalone value
+5. undefined=> Whose value we don't assign
+6. symbol=> unique
+*/
+
+//object---> Will discuss about object in later cases
+let age=22;
+console.log(typeof null); //type of null is object
+console.log(typeof undefined); // type of undefined is undefined
+console.log(typeof age); // type of age is number
