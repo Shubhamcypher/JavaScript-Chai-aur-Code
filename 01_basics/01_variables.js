a777259480ec635655cf2f344f9403f3bf05dd39
Shubhamcypher <79666913+Shubhamcypher@users.noreply.github.com>
2023-10-20 18:17:21 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14443
+let accountEmail="shubhamdevu023@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//  accountId=14475
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
