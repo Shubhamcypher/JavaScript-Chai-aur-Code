b8f3c4f2dde7aad9da0a1b69bac3ee7029fc032e
Shubhamcypher <79666913+Shubhamcypher@users.noreply.github.com>
2023-10-20 20:56:55 +0000

    Conversion Operations

---
+++ 01_basics/ConversionOperations.js
@@ -0,0 +1,25 @@
+let marks=33
+let result="33"
+let grade="33abc"
+let zero=null
+
+console.log(typeof marks);
+console.log(typeof result);
+console.log(typeof grade);
+console.log(typeof zero);
+
+let Nmarks=Number(marks)
+let Nresult=Number(result)
+let Ngrade=Number(grade)
+let Nzero=Number(zero)
+console.log("\n");
+
+console.log(typeof Nmarks);
+console.log(typeof Nresult);
+console.log(typeof Ngrade);
+console.log(typeof Nzero);
+
+console.log( Nmarks);
+console.log( Nresult);
+console.log( Ngrade);
+console.log( Nzero);
