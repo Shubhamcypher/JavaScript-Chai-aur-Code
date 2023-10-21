47a7a8d2e1eb469add157cc1496df52879f89115
Shubhamcypher <79666913+Shubhamcypher@users.noreply.github.com>
2023-10-21 09:22:33 +0000

    datatypes

--- 01_basics/ConversionOperations.js
+++ 01_basics/ConversionOperations.js
@@ -1,25 +1,62 @@
-let marks=33
-let result="33"
-let grade="33abc"
-let zero=null
+// let marks=33
+// let result="33"
+// let grade="33abc"
+// let zero=null
 
-console.log(typeof marks);
-console.log(typeof result);
-console.log(typeof grade);
-console.log(typeof zero);
+// console.log(typeof marks);
+// console.log(typeof result);
+// console.log(typeof grade);
+// console.log(typeof zero);
 
-let Nmarks=Number(marks)
-let Nresult=Number(result)
-let Ngrade=Number(grade)
-let Nzero=Number(zero)
-console.log("\n");
 
-console.log(typeof Nmarks);
-console.log(typeof Nresult);
-console.log(typeof Ngrade);
-console.log(typeof Nzero);
+// //Changing the datatypes to number
+// let Nmarks=Number(marks)
+// let Nresult=Number(result)
+// let Ngrade=Number(grade)
+// let Nzero=Number(zero)
+// console.log("\n");
 
-console.log( Nmarks);
-console.log( Nresult);
-console.log( Ngrade);
-console.log( Nzero);
+// console.log(typeof Nmarks);
+// console.log(typeof Nresult);
+// console.log(typeof Ngrade);
+// console.log(typeof Nzero);
+
+// console.log( Nmarks);
+// console.log( Nresult);
+// console.log( Ngrade);
+// console.log( Nzero);
+
+// // number
+// // string
+// // string
+// // object
+
+
+// // number
+// // number
+// // number
+// // number
+// // 33
+// // 33
+// // NaN----->Not a number
+
+// let score=null
+// let sScore=String(score)
+// console.log(typeof sScore);
+// console.log(sScore);
+
+console.log("2"==2); //normal check converts
+console.log("2"===2); //strict check dont convert and so gives value false
+//**************************************************************************************** */
+
+//Data Types Summary from interview perspective
+
+//Primitive--->The original value is not given
+//7Types--->String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+//Non Primitive or Reference type---->Will discuss it in stack and heap memory.
+//Array 
+let student=["shubham",1,false,null,""];
+console.log(student);
+// Object
+// Function
