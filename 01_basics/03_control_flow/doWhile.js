855999b72f3cd010037ef8c74a37b0fcbeff2937
Shubhamcypher <79666913+Shubhamcypher@users.noreply.github.com>
2023-11-01 07:58:09 +0000

    flow control

---
+++ 01_basics/03_control_flow/doWhile.js
@@ -0,0 +1,17 @@
+//do while Loop
+
+let score=2;
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+let points=11;
+
+do {
+    console.log(`point is ${points}`);
+    [points]++
+} while (points <= 10);
---
+++ 01_basics/03_control_flow/switch-case.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        
+    case "april":
+        console.log("april");
+        
+
+    default:
+        console.log("default case match");
+        break;
+}
+//not writing break will switch to every bottom case even to the default
